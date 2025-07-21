e62dbcf7167f990f44199ddcabeabc04651d940b
JACOB EDWARDS <jacob.john1@talktalk.net>
2025-07-21 10:55:07 +0100

    script

--- Luminance-GS/examples/basic.sh
+++ Luminance-GS/examples/basic.sh
@@ -14,14 +14,14 @@ do
     echo "Running $SCENE"
 
     # train without eval
-    CUDA_VISIBLE_DEVICES=0 python simple_trainer_ours.py default --disable_viewer --data_factor $DATA_FACTOR \
+    CUDA_VISIBLE_DEVICES=0 python simple_trainer_ours.py --disable_viewer --data_factor $DATA_FACTOR \
         --data_dir ../../../360_v2/$SCENE/ \
         --result_dir $RESULT_DIR/$SCENE/
 
     # run eval and render
     for CKPT in $RESULT_DIR/$SCENE/ckpts/*;
     do
-        CUDA_VISIBLE_DEVICES=0 python simple_trainer_ours.py default --disable_viewer --data_factor $DATA_FACTOR \
+        CUDA_VISIBLE_DEVICES=0 python simple_trainer_ours.py --disable_viewer --data_factor $DATA_FACTOR \
             --data_dir ../../../360_v2/$SCENE/ \
             --result_dir $RESULT_DIR/$SCENE/ \
             --ckpt $CKPT
